7383058e87b3ec9590cf1c5ef05d7d4400115eba
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-15 17:14:30 +0000

    change default web.php to json response

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app' => 'Jolly Jobs API',
+        'status' => 'OK',
+        'version' => '1.0.0'
+    ]);
 });
