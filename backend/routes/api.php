0a3a4596f0487c99141e9ace6a46b3ce4e0a6692
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-24 22:02:30 +0100

    implement routes register | login | logout | me

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,10 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+});
+
+
 
 Route::get('/test-connection', function () {
     return response()->json([
