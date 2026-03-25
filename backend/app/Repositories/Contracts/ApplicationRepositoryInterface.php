38f78f6047b91923641a5bf6e67d79ebc9e6bbe8
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-25 10:37:47 +0100

    ApplicationRepositoryInterface

---
+++ backend/app/Repositories/Contracts/ApplicationRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\Application;
+use Illuminate\Support\Collection;
+
+interface ApplicationRepositoryInterface
+{
+    public function all(): Collection;
+    public function findById(int $id): ?Application;
+    public function create(array $data): Application;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
+    public function findByJobSeeker(int $jobSeekerId): Collection;
+    public function findByJobOffer(int $jobOfferId): Collection;
+}
