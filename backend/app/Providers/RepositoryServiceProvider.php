5184703d8b7f14822f2a9460522533e84f8642f6
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-30 14:53:51 +0100

    RepositoryServiceProvider: bind Contract & Eloquent

--- backend/app/Providers/RepositoryServiceProvider.php
+++ backend/app/Providers/RepositoryServiceProvider.php
@@ -4,7 +4,26 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Contracts\UserRepositoryInterface;
-use \App\Repositories\Eloquent\UserRepository;
+use App\Repositories\Eloquent\UserRepository;
+use App\Repositories\Contracts\JobOfferRepositoryInterface;
+use App\Repositories\Eloquent\JobOfferRepository;
+use App\Repositories\Contracts\ApplicationRepositoryInterface;
+use App\Repositories\Eloquent\ApplicationRepository;
+use App\Repositories\Contracts\CityRepositoryInterface;
+use App\Repositories\Eloquent\CityRepository;
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Eloquent\CategoryRepository;
+use App\Repositories\Contracts\JobSeekerRepositoryInterface;
+use App\Repositories\Eloquent\JobSeekerRepository;
+use App\Repositories\Contracts\RecruiterRepositoryInterface;
+use App\Repositories\Eloquent\RecruiterRepository;
+use App\Repositories\Contracts\SkillRepositoryInterface;
+use App\Repositories\Eloquent\SkillRepository;
+use App\Repositories\Contracts\EducationRepositoryInterface;
+use App\Repositories\Eloquent\EducationRepository;
+use App\Repositories\Contracts\ExperienceRepositoryInterface;
+use App\Repositories\Eloquent\ExperienceRepository;
+
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -13,10 +32,17 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(
-            UserRepositoryInterface::class,
-            UserRepository::class
-        );
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(JobOfferRepositoryInterface::class, JobOfferRepository::class);
+        $this->app->bind(ApplicationRepositoryInterface::class, ApplicationRepository::class);
+        $this->app->bind(CityRepositoryInterface::class, CityRepository::class);
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(JobSeekerRepositoryInterface::class, JobSeekerRepository::class);
+        $this->app->bind(RecruiterRepositoryInterface::class, RecruiterRepository::class);
+        $this->app->bind(SkillRepositoryInterface::class, SkillRepository::class);
+        $this->app->bind(EducationRepositoryInterface::class, EducationRepository::class);
+        $this->app->bind(ExperienceRepositoryInterface::class, ExperienceRepository::class);
+        
     }
 
     /**
