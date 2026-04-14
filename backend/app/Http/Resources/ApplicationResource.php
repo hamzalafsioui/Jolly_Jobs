769b0fa88575d65f38b8432e02db6d62e01408fe
hamzalafsioui <hamzalafsioui@gmail.com>
2026-04-14 11:57:01 +0100

    upload job_seeker data & job_offer

--- backend/app/Http/Resources/ApplicationResource.php
+++ backend/app/Http/Resources/ApplicationResource.php
@@ -19,8 +19,13 @@ class ApplicationResource extends JsonResource
             'viewed_at'      => $this->viewed_at,
             'created_at'     => $this->created_at?->toISOString(),
             'updated_at'     => $this->updated_at?->toISOString(),
-            'job_seeker'     => new UserResource($this->whenLoaded('jobSeeker')),
-            // 'job_offer'      => new JobOfferResource($this->whenLoaded('jobOffer')), 
+            'job_seeker'     => $this->whenLoaded('jobSeeker', function() {
+                return [
+                    'id' => $this->jobSeeker->id,
+                    'user' => new UserResource($this->jobSeeker->user),
+                ];
+            }),
+            'job_offer'      => new JobOfferResource($this->whenLoaded('jobOffer')), 
         ];
     }
 }
