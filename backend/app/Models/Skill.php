20438659e7c05649b0f01b82518e0e0aaef2b2d7
hamzalafsioui <hamzalafsioui@gmail.com>
2026-04-17 10:22:13 +0100

    add category

--- backend/app/Models/Skill.php
+++ backend/app/Models/Skill.php
@@ -10,7 +10,12 @@ class Skill extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'category'];
+    protected $fillable = ['name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
     public function jobOffers(): BelongsToMany
     {
