069ee71d4e982d3d6af38b2b33b386aecdd25cec
hamzalafsioui <hamzalafsioui@gmail.com>
2026-04-17 10:22:30 +0100

    add foreignId category_id

--- backend/database/migrations/2026_03_16_204126_create_skills_table.php
+++ backend/database/migrations/2026_03_16_204126_create_skills_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('category')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
             $table->timestamps();
         });
     }
