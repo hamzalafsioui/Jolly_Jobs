7a5eadfb9d235317f5e5a5b188eb6accfb35270f
hamzalafsioui <hamzalafsioui@gmail.com>
2026-04-19 23:00:02 +0100

    add google routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,6 +26,8 @@ Broadcast::routes(['middleware' => ['auth:sanctum']]);
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/google/url', [AuthController::class, 'getGoogleAuthUrl']);
+    Route::post('/google/callback', [AuthController::class, 'handleGoogleCallback']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
     
